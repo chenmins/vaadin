63e87f9c668cc0b743609f263f3425933a222070
start.vaadin.com <no-reply@vaadin.com>
2021-10-10 14:34:12 +0000

    Generated project

---
+++ src/main/java/org/chenmin/va/data/service/SamplePersonRepository.java
@@ -0,0 +1,10 @@
+package org.chenmin.va.data.service;
+
+import org.chenmin.va.data.entity.SamplePerson;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, Integer> {
+
+}
---
+++ src/main/java/org/chenmin/va/data/service/UserService.java
@@ -0,0 +1,29 @@
+package org.chenmin.va.data.service;
+
+import org.chenmin.va.data.entity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.vaadin.artur.helpers.CrudService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Set;
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
+import org.chenmin.va.data.Role;
+import javax.persistence.Lob;
+
+@Service
+public class UserService extends CrudService<User, Integer> {
+
+    private UserRepository repository;
+
+    public UserService(@Autowired UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected UserRepository getRepository() {
+        return repository;
+    }
+
+}
